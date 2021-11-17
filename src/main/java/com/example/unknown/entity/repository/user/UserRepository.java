6340717be4f6c2f2d9039172e1072555ca3201f6
sehyoun <80021665+lyutvs@users.noreply.github.com>
2021-11-17 22:09:48 +0900

    Revert "[#4] 🔀 :: 피처 브런치 에서 커밋 확인"
    This reverts commit 81729a9dabfbebfd3231601ff0eb2f08bba5169b.

--- src/main/java/com/example/unknown/entity/repository/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.unknown.entity.repository.user;
-
-import com.example.unknown.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    
-}
--- src/main/java/com/example/unknown/entity/user/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.unknown.entity.user;
-
-public enum Role {
-
-    ROLE_USER, ROLE_ADMIN
-
-}
