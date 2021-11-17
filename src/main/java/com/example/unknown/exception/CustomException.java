1632a056a4e8779cb57cfaf0e510aa40dc21a882
세현 <lyutvs@gmail.com>
2021-11-17 23:27:30 +0900

    ⚡ :: 프로젝트 전체에서 사용할 exceptoin 생성

--- src/main/java/com/example/unknown/exception/CustomException.java
+++ src/main/java/com/example/unknown/exception/CustomException.java
@@ -1,4 +1,16 @@
 package com.example.unknown.exception;
 
-public class  {
+import com.example.unknown.util.HttpStatusResponseEntity;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class CustomException extends RuntimeException{
+
+    private final HttpStatusResponseEntity httpStatusResponseEntity;
+
+    public CustomException(ResponseEntity<HttpStatus> httpStatusResponseEntity) {
+        this.httpStatusResponseEntity = httpStatusResponseEntity;
+    }
 }
