390cf8197df1be85da117d744dca1e2c1ac78201
세현 <lyutvs@gmail.com>
2021-11-17 15:49:37 +0900

    ⚡ :: User 엔티티 추가 및 사용자 권한 설정

--- src/main/java/com/example/unknown/entity/user/User.java
+++ src/main/java/com/example/unknown/entity/user/User.java
@@ -1,2 +1,37 @@
-package com.example.unknown.entity.user;public class User {
+package com.example.unknown.entity.user;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "unknown-user")
+public class User {
+
+    @Id
+    @Column(length = 4)
+    private String name;
+
+    @Column(unique = false)
+    private String password;
+
+    @Column(length = 20)
+    private String email;
+
+    @Column(length = 20)
+    private Role role;
+
+    @Builder
+    private User(String name, String password, String email) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+
+
+
 }
