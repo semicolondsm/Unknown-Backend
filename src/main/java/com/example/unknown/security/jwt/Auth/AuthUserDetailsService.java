2f6064b4479aae3506046c583a83b1404f31dd60
세현 <lyutvs@gmail.com>
2021-11-17 23:29:02 +0900

    ⚡ :: AtuthUSerDetailService

--- src/main/java/com/example/unknown/security/jwt/Auth/AuthUserDetailsService.java
+++ src/main/java/com/example/unknown/security/jwt/Auth/AuthUserDetailsService.java
@@ -1,6 +1,22 @@
 package com.example.unknown.security.jwt.Auth;
 
+import com.example.unknown.entity.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UserDetailsService implements UserDetailsService{
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(UsernameNotFoundException::new);
+
+    }
 }
