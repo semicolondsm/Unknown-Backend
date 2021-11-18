52a52a68dc4988acde6a13273679c5bdd19a6222
세현 <lyutvs@gmail.com>
2021-11-18 13:55:08 +0900

    ⚡ :: 토큰 필터랑 config 구현

--- src/main/java/com/example/unknown/security/jwt/Auth/TokenFilter.java
+++ src/main/java/com/example/unknown/security/jwt/Auth/TokenFilter.java
@@ -1,2 +1,33 @@
-package com.example.unknown.security.jwt.Auth;public class TokenFilte {
+package com.example.unknown.security.jwt.Auth;
+
+import com.example.unknown.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class TokenFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(request);
+
+        if (token != null && jwtProvider.validateToken(token)) {
+            Authentication authentication = jwtProvider.authentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+        filterChain.doFilter(request, response);
+    }
 }
