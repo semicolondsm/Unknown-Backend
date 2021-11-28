8b365017ad81d78914144f89d92a124a55257053
세현 <lyutvs@gmail.com>
2021-11-28 14:56:18 +0900

    ⚡ :: 게시물 추가

--- src/main/java/com/example/unknown/controller/PostController.java
+++ src/main/java/com/example/unknown/controller/PostController.java
@@ -1,13 +1,22 @@
 package com.example.unknown.controller;
 
+import com.example.unknown.dto.request.PostRequest;
 import com.example.unknown.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void creatPost(@RequestBody PostRequest postRequest) {
+        postService.createNewPost(postRequest);
+    }
 
 }
--- src/main/java/com/example/unknown/service/PostService.java
+++ src/main/java/com/example/unknown/service/PostService.java
@@ -5,5 +5,5 @@ import com.example.unknown.dto.request.PostRequest;
 public interface PostService {
 
     void createNewPost(PostRequest postRequest);
-    
+
 }
