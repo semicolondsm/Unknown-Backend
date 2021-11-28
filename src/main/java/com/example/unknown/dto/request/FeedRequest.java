8bc5b017e52897bfe430f4c7626f1d669feb3c3b
KimJiMin <minmin4736@naver.com>
2021-11-28 22:32:19 +0900

    📑 :: FeedRequest 생성

---
+++ src/main/java/com/example/unknown/dto/request/FeedRequest.java
@@ -0,0 +1,17 @@
+package com.example.unknown.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@AllArgsConstructor
+public class FeedRequest {
+
+    @NotEmpty
+    private String title;
+
+    @Lob
+    private String description;
+}
