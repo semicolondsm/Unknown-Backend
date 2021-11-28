205ef18ca574eb491474402fe74582373984ff62
세현 <lyutvs@gmail.com>
2021-11-28 13:20:50 +0900

    📑 :: PostController 파일 생성

---
+++ src/main/java/com/example/unknown/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.example.unknown.controller;
+
+import com.example.unknown.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+}
