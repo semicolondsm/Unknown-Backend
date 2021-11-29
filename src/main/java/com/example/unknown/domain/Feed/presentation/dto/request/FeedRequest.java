36f77ecbbe20e97ab14033a1f193952fc185e791
KimJiMin <minmin4736@naver.com>
2021-11-29 23:11:37 +0900

    ⚡ :: 이름 변경

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/FeedRequest.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/FeedRequest.java
@@ -3,10 +3,20 @@ package com.example.unknown.domain.Feed.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
-public class PostRequest {
+public class FeedRequest {
 
+
+    @NotBlank
     private String title;
+
+    @Lob
     private String description;
+
+
 }
