b5d26fe71ab2c29e8a3e462ea146ecc75fc1dc99
KimJiMin <minmin4736@naver.com>
2021-11-29 23:14:38 +0900

    📑 :: FeedRepository 생성

---
+++ src/main/java/com/example/unknown/domain/Feed/domain/repository/FeedRepository.java
@@ -0,0 +1,11 @@
+package com.example.unknown.domain.Feed.domain.repository;
+
+import com.example.unknown.domain.Feed.domain.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+    Optional<Feed> findById(Long id);
+}
