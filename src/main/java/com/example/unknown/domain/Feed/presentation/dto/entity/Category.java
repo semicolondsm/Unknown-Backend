83ede7ae73f59cc69639f47d41dbde235471f7d0
KimJiMin <minmin4736@naver.com>
2021-11-29 23:13:06 +0900

    📑 :: Category 생성

---
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/entity/Category.java
@@ -0,0 +1,23 @@
+package com.example.unknown.domain.Feed.presentation.dto.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "unknown_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long id;
+
+    @Column(nullable = false)
+    private String language;
+
+    @Column(nullable = false)
+    private String subject;
+}
