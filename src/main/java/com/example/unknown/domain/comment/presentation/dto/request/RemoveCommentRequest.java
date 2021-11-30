127de7b053784d790cab69bd42ea7dbfddde6efb
세현 <lyutvs@gmail.com>
2021-12-01 00:42:19 +0900

    ⚡ :: RemoveCommentRequest

--- src/main/java/com/example/unknown/domain/comment/presentation/dto/request/RemoveCommentRequest.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/dto/request/RemoveCommentRequest.java
@@ -1,2 +1,12 @@
-package com.example.unknown.domain.comment.presentation.dto.request;public class RemoveCommentRequest {
+package com.example.unknown.domain.comment.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RemoveCommentRequest {
+
+    private long comment_id;
+    private String comment;
 }
