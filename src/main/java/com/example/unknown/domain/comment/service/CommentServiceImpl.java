f75cd71955b02ca3059c208e952da1fdc95b0813
세현 <lyutvs@gmail.com>
2021-11-30 21:47:26 +0900

    ⚡ :: 댓글 서비스

--- src/main/java/com/example/unknown/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/example/unknown/domain/comment/service/CommentServiceImpl.java
@@ -1,2 +1,33 @@
-package com.example.unknown.domain.comment.service;public class CommentServiceImpl {
+package com.example.unknown.domain.comment.service;
+
+import com.example.unknown.domain.User.domain.User;
+import com.example.unknown.domain.User.facade.UserFacade;
+import com.example.unknown.domain.comment.domain.Comment;
+import com.example.unknown.domain.comment.domain.repository.CommentRepository;
+import com.example.unknown.domain.comment.presentation.dto.CommentRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService{
+
+    private final CommentRepository commentRepository;
+    private final UserFacade userFacade;
+
+    @Override
+    public void postComment(CommentRequest request) {
+
+        User user = userFacade.getUserById(request.getComment());
+
+        commentRepository.save(
+                Comment.builder()
+                        .user(user)
+                        .content(request.getComment())
+                        .build()
+        );
+    }
+
+
+
 }
