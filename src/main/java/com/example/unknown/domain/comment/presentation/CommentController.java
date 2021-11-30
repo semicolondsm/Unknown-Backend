46659726f57b5bf84ddf1ab2aa350cadd693ddc4
세현 <lyutvs@gmail.com>
2021-12-01 00:39:00 +0900

    ⚡ :: /feeds/edit/comment api

--- src/main/java/com/example/unknown/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/CommentController.java
@@ -1,13 +1,14 @@
 package com.example.unknown.domain.comment.presentation;
 
 import com.example.unknown.domain.comment.presentation.dto.request.CommentRequest;
+import com.example.unknown.domain.comment.presentation.dto.request.EditCommentRequest;
 import com.example.unknown.domain.comment.presentation.dto.response.CommentResponse;
 import com.example.unknown.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/feeds")
@@ -17,8 +18,16 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/comment")
-    public CommentResponse postComment(@RequestBody CommentRequest request) {
-       return commentService.postComment(request);
+    public void postComment(@RequestBody @Valid CommentRequest request) {
+
+        commentService.postComment(request);
+    }
+
+    @PostMapping("/edit/comment")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void editComment(@RequestBody @Valid EditCommentRequest request) {
+
+        commentService.editComment(request);
     }
 
 }
