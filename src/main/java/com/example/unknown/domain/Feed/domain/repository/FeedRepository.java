e735365c45c3b9addc40a019a107ed4e50908a74
KimJiMin <minmin4736@naver.com>
2021-11-30 20:24:31 +0900

    ⚡ :: Optional 추가

--- src/main/java/com/example/unknown/domain/Feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/unknown/domain/Feed/domain/repository/FeedRepository.java
@@ -3,5 +3,10 @@ package com.example.unknown.domain.Feed.domain.repository;
 import com.example.unknown.domain.Feed.domain.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+    Optional<Feed> findById(Long Id);
+
 }
