f3e01f35431009f8d16a10ecbeef3e00dad6ce0f
세현 <lyutvs@gmail.com>
2021-12-01 00:39:29 +0900

    ⚡ :: CommnetNotFoundException

--- src/main/java/com/example/unknown/domain/comment/exception/CommentNotFoundException.java
+++ src/main/java/com/example/unknown/domain/comment/exception/CommentNotFoundException.java
@@ -1,10 +1,15 @@
-package com.example.unknown.global.exception;
+package com.example.unknown.domain.comment.exception;
 
+import com.example.unknown.domain.User.exception.UserNotVerificationException;
 import com.example.unknown.global.error.exception.CustomException;
 import com.example.unknown.global.error.exception.ErrorCode;
 
 public class CommentNotFoundException extends CustomException {
-    public CommentNotFoundException(ErrorCode errorCode) {
+
+    public static CustomException EXCEPTION =
+            new CommentNotFoundException();
+
+    private CommentNotFoundException() {
         super(ErrorCode.COMMENT_NOT_FOUND);
     }
 }
