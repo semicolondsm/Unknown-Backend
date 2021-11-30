3b4c7ab6de0045145993e79cb0c168a0c337efbf
세현 <lyutvs@gmail.com>
2021-11-30 23:07:57 +0900

    ♻ :: 이름 변경

---
+++ src/main/java/com/example/unknown/domain/comment/presentation/dto/request/EditCommentRequest.java
@@ -0,0 +1,14 @@
+package com.example.unknown.domain.comment.presentation.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class EditCommentRepository {
+
+    
+}
