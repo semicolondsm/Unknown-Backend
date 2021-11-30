b85b5aa99924bc4ae8bef3cd84e531108d702795
세현 <lyutvs@gmail.com>
2021-11-30 22:40:15 +0900

    ⚡ :: CommentResponse

--- src/main/java/com/example/unknown/domain/comment/presentation/dto/response/CommentResponse.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/dto/response/CommentResponse.java
@@ -1,2 +1,24 @@
-package com.example.unknown.domain.comment.presentation.dto.response;public class CommentResponse {
+package com.example.unknown.domain.comment.presentation.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CommentResponse {
+
+    private final Long id;
+    private final String content;
+    private final String createDate;
+    private final String lastModifiedDate;
+
+    public CommentResponse(Long id, String content, String createDate, String lastModifiedDate) {
+        this.id = id;
+        this.content = content;
+        this.createDate = createDate;
+        this.lastModifiedDate = lastModifiedDate;
+
+    }
 }
+
