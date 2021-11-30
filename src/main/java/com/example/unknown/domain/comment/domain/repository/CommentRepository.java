28def4e1ac94f0dc388c5bca84f844b6d6d87fac
세현 <lyutvs@gmail.com>
2021-11-30 22:20:43 +0900

    ♻ :: @Repository 어노테이션 뻄

--- src/main/java/com/example/unknown/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/unknown/domain/comment/domain/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import com.example.unknown.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
