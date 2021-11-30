3a9b5370976bd1cc77cc04bf3b5211d01b9d83fc
세현 <lyutvs@gmail.com>
2021-11-30 18:10:40 +0900

    ⚡ :: Comment

--- src/main/java/com/example/unknown/domain/comment/domain/Comment.java
+++ src/main/java/com/example/unknown/domain/comment/domain/Comment.java
@@ -1,26 +1,37 @@
 package com.example.unknown.domain.comment.domain;
 
+import com.example.unknown.domain.User.domain.User;
 import lombok.*;
 
 import javax.persistence.*;
 
 
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "unknown-comment")
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    private Integer id;
+    @Column(name = "comment_id")
+        private Integer id;
 
-    @Lob
     @Column(columnDefinition="TEXT")
     private String comment;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Comment(String content, User user) {
+        this.user = user;
+        this.comment = content;
+    }
+
+    public void editContent(String comment) {
+        this.comment = comment;
+    }
 
 
 }
