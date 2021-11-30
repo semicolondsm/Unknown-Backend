fa90abc1d0686e89f379445b7b95de9c20dcd9db
세현 <lyutvs@gmail.com>
2021-11-30 19:12:39 +0900

    ⚡ :: CommentrRequest

--- src/main/java/com/example/unknown/domain/comment/presentation/dto/CommentRequest.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/dto/CommentRequest.java
@@ -1,2 +1,15 @@
-package com.example.unknown.domain.comment.presentation.dto;public class CommentRequest {
+package com.example.unknown.domain.comment.presentation.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CommentRequest {
+
+    private String comment;
+
 }
