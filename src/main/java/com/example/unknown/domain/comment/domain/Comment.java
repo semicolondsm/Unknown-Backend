44b01a2711a655ea903600d3cad9aa707033f995
세현 <lyutvs@gmail.com>
2021-11-30 22:20:27 +0900

    ♻ :: Long 으로 바꿈

--- src/main/java/com/example/unknown/domain/comment/domain/Comment.java
+++ src/main/java/com/example/unknown/domain/comment/domain/Comment.java
@@ -13,7 +13,7 @@ public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(columnDefinition = "TEXT")
     private String comment;
