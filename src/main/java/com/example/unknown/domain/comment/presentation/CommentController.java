8beb39244c1291b2abaaded5c0c9aad9d10ff582
세현 <lyutvs@gmail.com>
2021-11-30 19:12:20 +0900

    ⚡ :: /feeds/comment api

--- src/main/java/com/example/unknown/domain/comment/presentation/CommentController.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/CommentController.java
@@ -1,9 +1,26 @@
 package com.example.unknown.domain.comment.presentation;
 
+import com.example.unknown.domain.comment.presentation.dto.CommentRequest;
+import com.example.unknown.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-public class Controller {
+@RequestMapping("/feeds")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/comment")
+    public ResponseEntity postComment(@RequestBody CommentRequest request) {
+
+        commentService.postComment(request);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
