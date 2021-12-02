9e4d01784ec15481e173f6038f21a6ddb82af7b9
세현 <lyutvs@gmail.com>
2021-12-02 19:13:58 +0900

    ♻ :: 정규 표현식 추가

--- src/main/java/com/example/unknown/domain/User/presentation/dto/request/UserRequest.java
+++ src/main/java/com/example/unknown/domain/User/presentation/dto/request/UserRequest.java
@@ -1,20 +1,27 @@
 package com.example.unknown.domain.User.presentation.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Cleanup;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @AllArgsConstructor
 public class UserRequest {
 
-    @NotBlank
-    @Length(min = 6, max = 36)
+    @NotEmpty
+    @Email(message = "유효하지 않은 이메일 형식입니다.",
+            regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     private String email;
 
-    @NotBlank
-    @Length(min = 8, max = 25)
+    @NotEmpty
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     private String password;
 }
