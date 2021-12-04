2f0426d0a751f29a556535901f1b78fa4c25f1bc
세현 <lyutvs@gmail.com>
2021-12-05 03:30:49 +0900

    ⚡ :: Comment

--- src/main/java/com/example/unknown/domain/comment/domain/Comment.java
+++ src/main/java/com/example/unknown/domain/comment/domain/Comment.java
@@ -4,6 +4,8 @@ import com.example.unknown.domain.User.domain.User;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 
 @Getter
@@ -16,21 +18,26 @@ public class Comment {
     private Long commentId;
 
     @Column(columnDefinition = "TEXT")
-    private String comment;
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    private LocalDateTime createAt;
+
+    private LocalDateTime updateAt;
+
     @Builder
     public Comment(Long commentId, String content, User user) {
         this.commentId = commentId;
         this.user = user;
-        this.comment = content;
+        this.content= content;
     }
 
-    public void editContent(String comment) {
-        this.comment = comment;
+    public void editContent(String content) {
+        this.content = content;
+        this.updateAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
     }
 
 
