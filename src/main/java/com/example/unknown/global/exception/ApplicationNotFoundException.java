dd56b94b2402e046e7fa300a8c3e76eb06154a5c
세현 <lyutvs@gmail.com>
2021-12-05 02:09:28 +0900

    ⚡ :: APPLICATION_NOT_FOUND

--- src/main/java/com/example/unknown/global/exception/ApplicationNotFoundException.java
+++ src/main/java/com/example/unknown/global/exception/ApplicationNotFoundException.java
@@ -1,2 +1,14 @@
-package com.example.unknown.global.exception;public class ApplicationNotFoundException {
+package com.example.unknown.global.exception;
+
+import com.example.unknown.global.error.exception.CustomException;
+import com.example.unknown.global.error.exception.ErrorCode;
+
+public class ApplicationNotFoundException extends CustomException {
+
+    public static CustomException EXCEPTION =
+            new ApplicationNotFoundException();
+
+    private ApplicationNotFoundException() {
+        super(ErrorCode.APPLICATION_NOT_FOUND);
+    }
 }
