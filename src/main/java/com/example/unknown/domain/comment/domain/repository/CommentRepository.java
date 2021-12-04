1bcc3e993c0b6b4d4b5a2563b990756dd7da2373
세현 <lyutvs@gmail.com>
2021-12-05 03:31:00 +0900

    ⚡ :: CommentRepository

--- src/main/java/com/example/unknown/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/unknown/domain/comment/domain/repository/CommentRepository.java
@@ -4,7 +4,9 @@ import com.example.unknown.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
+    List<Comment> findAllByFeed(Integer feedId);
 }
