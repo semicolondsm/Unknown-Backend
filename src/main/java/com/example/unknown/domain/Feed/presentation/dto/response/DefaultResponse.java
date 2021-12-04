f408874adac533a73232e997123acd14fb3bc98f
세현 <lyutvs@gmail.com>
2021-12-05 03:32:18 +0900

    ⚡ :: DefaultResponse

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/response/DefaultResponse.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/response/DefaultResponse.java
@@ -1,4 +1,12 @@
 package com.example.unknown.domain.Feed.presentation.dto.response;
 
-public class DefultResponse {
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DefaultResponse {
+
+    private final Long id;
+    private final String tittle;
 }
