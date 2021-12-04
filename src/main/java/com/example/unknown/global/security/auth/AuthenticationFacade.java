6ed853a2b4a9385b4b2cd3bb97d23ad3e50efaa2
세현 <lyutvs@gmail.com>
2021-12-05 03:33:16 +0900

    ⚡ :: AuthenticationFacade

--- src/main/java/com/example/unknown/global/security/auth/AuthenticationFacade.java
+++ src/main/java/com/example/unknown/global/security/auth/AuthenticationFacade.java
@@ -1,2 +1,19 @@
-package com.example.unknown.global.security.auth;public class AuthenticationFacade {
+package com.example.unknown.global.security.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade {
+
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public String getUserEmail() {
+        return this.getAuthentication().getName();
+    }
+
+
 }
