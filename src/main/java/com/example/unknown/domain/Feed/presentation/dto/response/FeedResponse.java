a52a2b978f7495a03d062ec52502c11b00fdc528
세현 <lyutvs@gmail.com>
2021-12-04 17:28:34 +0900

    ⚡ :: FeedResponse 작성

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/response/FeedResponse.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/response/FeedResponse.java
@@ -1,2 +1,23 @@
-package com.example.unknown.domain.Feed.presentation.dto.response;public class FeedResponse {
+package com.example.unknown.domain.Feed.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class FeedResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd`T`hh:mm:SS")
+    private LocalDateTime createdTime;
+
+    private String userName;
 }
