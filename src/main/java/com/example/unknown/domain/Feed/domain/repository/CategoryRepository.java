aba6f433c4146132663c6588342662f812d438a0
세현 <lyutvs@gmail.com>
2021-12-06 22:56:45 +0900

    ⚡ :: CategoryRepository

--- src/main/java/com/example/unknown/domain/Feed/domain/repository/CategoryRepository.java
+++ src/main/java/com/example/unknown/domain/Feed/domain/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.example.unknown.domain.Feed.domain.repository;
 import com.example.unknown.domain.Feed.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findCategoryBycategoryName(String categoryName);
 }
