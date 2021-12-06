ee95a792c046ed0c960edf4f5b787c2429a559ea
세현 <lyutvs@gmail.com>
2021-12-06 22:57:24 +0900

    ⚡ :: CategoryServiceImpl

--- src/main/java/com/example/unknown/domain/Feed/service/CategoryServiceImpl.java
+++ src/main/java/com/example/unknown/domain/Feed/service/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.unknown.domain.Feed.service;
 
 import com.example.unknown.domain.Feed.domain.Category;
 import com.example.unknown.domain.Feed.domain.repository.CategoryRepository;
+import com.example.unknown.domain.Feed.exception.CategoryNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,11 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Override
-    public List<Category> getCategory() {
-        return categoryRepository.findAll();
+    public Category findCategoryByName(String categoryName) {
+
+            return categoryRepository.findCategoryBycategoryName(categoryName)
+                    .orElseThrow(() -> CategoryNotFoundException.EXCEPTION);
+
     }
+
 }
