ce12d526bf4378755d9bcc76d4cbb58ac3594a59
세현 <lyutvs@gmail.com>
2021-12-06 22:57:00 +0900

    ⚡ :: CategoryNotFoundException

--- src/main/java/com/example/unknown/domain/Feed/exception/CategoryNotFoundException.java
+++ src/main/java/com/example/unknown/domain/Feed/exception/CategoryNotFoundException.java
@@ -1,2 +1,15 @@
-package com.example.unknown.domain.Feed.exception;public class CategoryNotfoundException {
+package com.example.unknown.domain.Feed.exception;
+
+import com.example.unknown.global.error.exception.CustomException;
+import com.example.unknown.global.error.exception.ErrorCode;
+
+public class CategoryNotFoundException extends CustomException {
+
+    public static CustomException EXCEPTION =
+            new CategoryNotFoundException();
+
+    private CategoryNotFoundException() {
+        super(ErrorCode.CATEGORY_NOT_FOUND);
+    }
+
 }
