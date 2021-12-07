e1bc2d3d687c53a632729031b1b7b60c006ebbaf
세현 <lyutvs@gmail.com>
2021-12-07 18:40:32 +0900

    ⚡ :: FeedService

--- src/main/java/com/example/unknown/domain/Feed/service/FeedService.java
+++ src/main/java/com/example/unknown/domain/Feed/service/FeedService.java
@@ -1,22 +1,8 @@
 package com.example.unknown.domain.Feed.service;
 
-import com.example.unknown.domain.Feed.domain.Feed;
-import com.example.unknown.domain.Feed.presentation.dto.request.CreateFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.DeleteFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateDescriptionRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateTitleRequest;
-
-import java.util.List;
+import com.example.unknown.domain.Feed.presentation.dto.request.PostRequest;
 
 public interface FeedService {
 
-    List<Feed> getFeed();
-
-    void createFeed(CreateFeedRequest request);
-
-    void updateTitle(UpdateTitleRequest request);
-
-    void updateDescription(UpdateDescriptionRequest request);
-
-    void deleteFeed(DeleteFeedRequest request);
+    void PostFeed(PostRequest request);
 }
