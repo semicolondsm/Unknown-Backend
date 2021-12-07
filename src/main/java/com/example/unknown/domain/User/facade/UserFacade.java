08cb8ad16582e12d758171f1b6760925a2d08bec
세현 <lyutvs@gmail.com>
2021-12-07 22:10:14 +0900

    ⚡ :: 중복 코드 및 email

--- src/main/java/com/example/unknown/domain/User/facade/UserFacade.java
+++ src/main/java/com/example/unknown/domain/User/facade/UserFacade.java
@@ -2,10 +2,13 @@ package com.example.unknown.domain.User.facade;
 
 import com.example.unknown.domain.User.domain.User;
 import com.example.unknown.domain.User.domain.repository.UserRepository;
+import com.example.unknown.domain.User.exception.CertificateNotFoundException;
 import com.example.unknown.domain.User.exception.UserNotFoundException;
 import com.example.unknown.global.exception.UserExistsException;
 import com.example.unknown.global.exception.UserNotExistsException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +17,16 @@ public class UserFacade {
 
     private final UserRepository userRepository;
 
-    public User getUserById(String email) {
-        return userRepository.findById(email).orElseThrow(() -> UserNotExistsException.EXCEPTION);
+    public String getEmail() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication.getPrincipal() == null)
+            throw CertificateNotFoundException.EXCEPTION;
+
+        return authentication.getName();
+    }
+    public User getUser() {
+        return userRepository.findById(getEmail())
+                .orElseThrow(() -> CertificateNotFoundException.EXCEPTION);
     }
 
     public void isAlreadyExists(String email) {
@@ -23,10 +34,4 @@ public class UserFacade {
             throw UserExistsException.EXCEPTION;
         }
     }
-
-    public User getUserByEmail(String email){
-        return userRepository.findById(email)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-    }
-
 }
