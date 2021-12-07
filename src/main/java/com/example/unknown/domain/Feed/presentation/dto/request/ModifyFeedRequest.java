6ac001df97e873b1e62c63ab31b6d618a1c5634a
세현 <lyutvs@gmail.com>
2021-12-07 22:10:38 +0900

    ⚡ :: ModifyFeedRequest

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/ModifyFeedRequest.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/ModifyFeedRequest.java
@@ -1,2 +1,24 @@
-package com.example.unknown.domain.Feed.presentation.dto.request;public class ModifyFeed {
+package com.example.unknown.domain.Feed.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class ModifyFeedRequest {
+
+    @NotNull(message = "Feed_id는 없으면 안됩니다.")
+    private Long feedId;
+
+    @Size(max = 30, message = "title은 30자를 넘으면 안됩니다.")
+    @NotBlank(message = "title은 공백이면 안됩니다.")
+    private String title;
+
+    @Size(max = 65535, message = "description은 655353글자를 넘으면 안됩니다.")
+    private String description;
+
 }
