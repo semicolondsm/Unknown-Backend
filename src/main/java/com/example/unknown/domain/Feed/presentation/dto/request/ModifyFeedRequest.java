b78ae52097fb3a609861fa5122d80b210e58a145
세현 <lyutvs@gmail.com>
2021-12-07 23:29:03 +0900

    ⚡ :: ModifyFeedRequest

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/ModifyFeedRequest.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/ModifyFeedRequest.java
@@ -2,10 +2,12 @@ package com.example.unknown.domain.Feed.presentation.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -20,5 +22,8 @@ public class ModifyFeedRequest {
 
     @Size(max = 65535, message = "description은 65535글자를 넘으면 안됩니다.")
     private String description;
+
+    @DateTimeFormat(pattern = "YYYY-MM-DD'T'hh:mm:ss")
+    private LocalDateTime modifyTime;
 
 }
