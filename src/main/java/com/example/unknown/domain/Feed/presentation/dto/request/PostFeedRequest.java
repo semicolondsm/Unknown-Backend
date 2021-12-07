70e0fd3e41515bb2f1c0e1d77ea7b0e9faa3b90b
세현 <lyutvs@gmail.com>
2021-12-07 23:29:14 +0900

    ⚡ :: PostFeedRequest

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/PostFeedRequest.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/PostFeedRequest.java
@@ -2,9 +2,11 @@ package com.example.unknown.domain.Feed.presentation.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -17,4 +19,9 @@ public class PostFeedRequest {
     @Size(max = 65535, message = "description은 65535글자를 넘어서는 안됩니다.")
     @NotBlank(message = "description은 비어이었으면 안됩니다.")
     private String description;
+
+    @DateTimeFormat(pattern = "YYYY-MM-DD'T'hh:mm:ss")
+    private LocalDateTime createdTime;
+
+
 }
