bce7b776717679ed58a6cbb39dd2daceff452da4
세현 <lyutvs@gmail.com>
2021-12-07 22:51:11 +0900

    ⚡ :: removeFeed

--- src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.unknown.domain.Feed.service;
 
 import com.example.unknown.domain.Feed.domain.Feed;
 import com.example.unknown.domain.Feed.domain.repository.FeedRepository;
+import com.example.unknown.domain.Feed.exception.FeedNotExistsException;
 import com.example.unknown.domain.Feed.facade.FeedFacade;
 import com.example.unknown.domain.Feed.presentation.dto.request.ModifyFeedRequest;
 import com.example.unknown.domain.Feed.presentation.dto.request.PostFeedRequest;
 import com.example.unknown.domain.User.domain.User;
 import com.example.unknown.domain.User.domain.repository.UserRepository;
+import com.example.unknown.domain.User.exception.UserNotFoundException;
 import com.example.unknown.domain.User.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class FeedServiceImpl implements FeedService {
 
+    private final UserRepository userRepository;
     private final FeedRepository feedRepository;
     private final UserFacade userFacade;
     private final FeedFacade feedFacade;
@@ -41,5 +44,16 @@ public class FeedServiceImpl implements FeedService {
         feed
                 .changeTitle(request.getTitle())
                 .changeDescription(request.getDescription());
+    }
+
+    @Override
+    public void removeFeed(Long id) {
+        User user = userRepository.findById(userFacade.getEmail())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        Feed feed = feedRepository.findById(id)
+                .orElseThrow(() -> FeedNotExistsException.EXCEPTION);
+
+        feedRepository.deleteById(id);
     }
 }
