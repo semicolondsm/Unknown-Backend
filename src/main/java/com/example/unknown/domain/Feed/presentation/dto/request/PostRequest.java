b53e76bcfdc70f382779b736be05d097dc3236b5
세현 <lyutvs@gmail.com>
2021-12-07 18:40:04 +0900

    ⚡ ::  PostRequest 파일 생성

--- src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/PostRequest.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/PostRequest.java
@@ -1,2 +1,12 @@
-package com.example.unknown.domain.Feed.presentation.dto.request;public class PostRequest {
+package com.example.unknown.domain.Feed.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostRequest {
+
+    private String title;
+    private String description;
 }
