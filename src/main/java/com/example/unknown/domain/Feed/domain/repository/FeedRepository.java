17fd404d134332a89dc74dfa64d94b638cef0f88
세현 <lyutvs@gmail.com>
2021-12-07 23:22:21 +0900

    ⚡:: FeedRepository

--- src/main/java/com/example/unknown/domain/Feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/unknown/domain/Feed/domain/repository/FeedRepository.java
@@ -1,10 +1,18 @@
 package com.example.unknown.domain.Feed.domain.repository;
 
 import com.example.unknown.domain.Feed.domain.Feed;
+import com.example.unknown.domain.User.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     Optional<Feed> findFeedById(Long Id);
+    List<Feed> findById(User user);
+
+    Page<Feed> findFeedById(boolean page, PageRequest range);
 }
