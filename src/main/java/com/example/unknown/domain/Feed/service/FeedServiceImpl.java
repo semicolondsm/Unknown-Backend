44781b3441f1e20b3ba9937aacae0a8dfa8622da
세현 <lyutvs@gmail.com>
2021-12-07 22:11:15 +0900

    ⚡ :: FeedServiceImpl

--- src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
@@ -1,6 +1,13 @@
 package com.example.unknown.domain.Feed.service;
 
-import com.example.unknown.domain.Feed.presentation.dto.request.PostRequest;
+import com.example.unknown.domain.Feed.domain.Feed;
+import com.example.unknown.domain.Feed.domain.repository.FeedRepository;
+import com.example.unknown.domain.Feed.facade.FeedFacade;
+import com.example.unknown.domain.Feed.presentation.dto.request.ModifyFeedRequest;
+import com.example.unknown.domain.Feed.presentation.dto.request.PostFeedRequest;
+import com.example.unknown.domain.User.domain.User;
+import com.example.unknown.domain.User.domain.repository.UserRepository;
+import com.example.unknown.domain.User.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +15,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class FeedServiceImpl implements FeedService {
 
-    @Override
-    public void PostFeed(PostRequest request) {
+    private final UserRepository userRepository;
+    private final FeedRepository feedRepository;
+    private final UserFacade userFacade;
+    private final FeedFacade feedFacade;
 
+    @Override
+    public void PostFeed(PostFeedRequest request) {
+
+        User user = userFacade.getUser();
+
+        Feed feed = feedRepository.save(
+                        Feed.builder()
+                                .title(request.getTitle())
+                                .description(request.getDescription())
+                                .build()
+        );
+    }
+
+    @Override
+    public void modifyFeed(ModifyFeedRequest request) {
+        User user = userFacade.getUser();
+
+        Feed feed = feedFacade.getFeedById(request.getFeedId());
+
+        feed
+                .changeTitle(request.getTitle())
+                .changeDescription(request.getDescription());
     }
 }
