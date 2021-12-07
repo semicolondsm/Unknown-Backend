3285c800ce6ef0f04fbb663356e20072483f2d8d
세현 <lyutvs@gmail.com>
2021-12-07 21:42:45 +0900

    📑 :: ModifyFeedRequest 파일 생성

---
+++ src/main/java/com/example/unknown/domain/Feed/presentation/dto/request/PostFeedRequest.java
@@ -0,0 +1,20 @@
+package com.example.unknown.domain.Feed.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class PostRequest {
+
+    @Size(max = 30, message = "title은 30글자를 넘으면 안됩니다.")
+    @NotBlank(message = "title은 비어있으면 안됩니다.")
+    private String title;
+
+    @Size(max = 65535, message = "description은 65535글자를 넘어서는 안됩니다.")
+    @NotBlank(message = "description은 비어이었으면 안됩니다.")
+    private String description;
+}
