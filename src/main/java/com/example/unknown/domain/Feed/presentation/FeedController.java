e23c6f8faa463f0b92b4a7082a1abf080c54b707
세현 <lyutvs@gmail.com>
2021-12-07 23:22:32 +0900

    ⚡ :: FeedController

--- src/main/java/com/example/unknown/domain/Feed/presentation/FeedController.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/FeedController.java
@@ -2,12 +2,14 @@ package com.example.unknown.domain.Feed.presentation;
 
 import com.example.unknown.domain.Feed.presentation.dto.request.ModifyFeedRequest;
 import com.example.unknown.domain.Feed.presentation.dto.request.PostFeedRequest;
+import com.example.unknown.domain.Feed.presentation.dto.response.FeedResponse;
 import com.example.unknown.domain.Feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/feed")
@@ -33,6 +35,11 @@ public class FeedController {
     public void removeFeed(@PathVariable(name = "feed_id") Long feedId) {
 
         feedService.removeFeed(feedId);
+    }
+
+    @GetMapping("/List")
+    public List<FeedResponse> getFeed(@RequestParam("page")int page, @RequestParam("range") int range) {
+        return feedService.getFeed(page, range);
     }
 
 }
