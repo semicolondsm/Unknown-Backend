1726a16edaa34e80bf59926b45df349a57486039
세현 <lyutvs@gmail.com>
2021-12-07 18:40:15 +0900

    ⚡ :: FeedController

--- src/main/java/com/example/unknown/domain/Feed/presentation/FeedController.java
+++ src/main/java/com/example/unknown/domain/Feed/presentation/FeedController.java
@@ -1,16 +1,13 @@
 package com.example.unknown.domain.Feed.presentation;
 
-import com.example.unknown.domain.Feed.domain.Feed;
-import com.example.unknown.domain.Feed.presentation.dto.request.CreateFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.DeleteFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateDescriptionRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateTitleRequest;
+import com.example.unknown.domain.Feed.presentation.dto.request.PostRequest;
 import com.example.unknown.domain.Feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,28 +16,9 @@ public class FeedController {
 
     private final FeedService feedService;
 
-    @PostMapping("/create")
-    public void createFeed(@RequestBody @Valid CreateFeedRequest request) {
-        feedService.createFeed(request);
-    }
-
-    @GetMapping
-    public List<Feed> get() {
-        return feedService.getFeed();
-    }
-
-    @PutMapping("/update/title")
-    public void updateTitle(@RequestBody @Valid UpdateTitleRequest request) {
-        feedService.updateTitle(request);
-    }
-
-    @PutMapping("/update/description")
-    public void updateDescription(@RequestBody @Valid UpdateDescriptionRequest request) {
-        feedService.updateDescription(request);
-    }
-
-    @DeleteMapping("/delete")
-    public void deleteFeed(@RequestBody @Valid DeleteFeedRequest request) {
-        feedService.deleteFeed(request);
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void PostFeed (PostRequest request) {
+        feedService.PostFeed(request);
     }
 }
