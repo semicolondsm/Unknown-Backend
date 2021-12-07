d6ae920902a505b33fc39b20313ed8618492d5fe
세현 <lyutvs@gmail.com>
2021-12-07 18:40:47 +0900

    ⚡ :: FeedServiceImpl

--- src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/unknown/domain/Feed/service/FeedServiceImpl.java
@@ -1,70 +1,15 @@
 package com.example.unknown.domain.Feed.service;
 
-import com.example.unknown.domain.Feed.domain.Feed;
-import com.example.unknown.domain.Feed.domain.repository.FeedRepository;
-import com.example.unknown.domain.Feed.facade.FeedFacade;
-import com.example.unknown.domain.Feed.presentation.dto.request.CreateFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.DeleteFeedRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateDescriptionRequest;
-import com.example.unknown.domain.Feed.presentation.dto.request.UpdateTitleRequest;
+import com.example.unknown.domain.Feed.presentation.dto.request.PostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class FeedServiceImpl implements FeedService {
 
-    private final FeedFacade feedFacade;
-    private final FeedRepository feedRepository;
-
     @Override
-    public List<Feed> getFeed() {
-        return feedRepository.findAll();
-    }
+    public void PostFeed(PostRequest request) {
 
-    @Override
-    public void createFeed(CreateFeedRequest request) {
-
-        feedRepository.save(Feed.builder()
-                .title(request.getTitle())
-                .description(request.getDescription())
-                .build());
-    }
-
-    @Transactional
-    @Override
-    public void updateTitle(UpdateTitleRequest request) {
-
-        Feed feed = feedFacade.getFeedById(request.getId());
-
-        feedRepository.save(Feed.builder()
-                .id(feed.getId())
-                .title(request.getTitle())
-                .build());
-    }
-
-    @Transactional
-    @Override
-    public void updateDescription(UpdateDescriptionRequest request) {
-
-        Feed feed = feedFacade.getFeedById(request.getId());
-
-        feedRepository.save(Feed.builder()
-                .id(feed.getId())
-                .description(request.getDescription())
-                .build());
-    }
-
-    @Override
-    public void deleteFeed(DeleteFeedRequest request) {
-
-        Feed feed = feedFacade.getFeedById(request.getId());
-
-        feedRepository.delete(Feed.builder()
-                .id(feed.getId())
-                .build());
     }
 }
