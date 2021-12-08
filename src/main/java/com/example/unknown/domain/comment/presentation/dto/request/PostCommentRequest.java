3702440013c635c5a03d1a5fa43cd67c9f20ec76
세현 <lyutvs@gmail.com>
2021-12-08 19:02:41 +0900

    ♻ :: 이름 변경

--- src/main/java/com/example/unknown/domain/comment/presentation/dto/request/PostCommentRequest.java
+++ src/main/java/com/example/unknown/domain/comment/presentation/dto/request/PostCommentRequest.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class CommentRequest {
+public class PostCommentRequest {
 
     private String comment;
 
