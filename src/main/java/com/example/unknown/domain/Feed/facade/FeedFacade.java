5a790677404107911c1cb84a08840f43841b74c0
세현 <lyutvs@gmail.com>
2021-12-08 13:29:57 +0900

    ⚡ :: FeedFacade

--- src/main/java/com/example/unknown/domain/Feed/facade/FeedFacade.java
+++ src/main/java/com/example/unknown/domain/Feed/facade/FeedFacade.java
@@ -3,6 +3,8 @@ package com.example.unknown.domain.Feed.facade;
 import com.example.unknown.domain.Feed.domain.Feed;
 import com.example.unknown.domain.Feed.domain.repository.FeedRepository;
 import com.example.unknown.domain.Feed.exception.FeedNotExistsException;
+import com.example.unknown.domain.Feed.presentation.dto.response.FeedResponse;
+import com.example.unknown.domain.User.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +17,15 @@ public class FeedFacade {
     public Feed getFeedById(Long feedId) {
         return feedRepository.findFeedById(feedId)
                 .orElseThrow(() -> FeedNotExistsException.EXCEPTION);
+    }
+
+    public FeedResponse feedToFeedResponse (Feed feed, User user) {
+
+        FeedResponse feedResponse = FeedResponse.builder()
+                .title(feed.getTitle())
+                .description(feed.getDescription())
+                .build();
+
+        return feedResponse;
     }
 }
