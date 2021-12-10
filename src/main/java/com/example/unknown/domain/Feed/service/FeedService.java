6a1cce8817cc30d573771208a855d2e62cd8e5c1
세현 <lyutvs@gmail.com>
2021-12-10 22:52:25 +0900

    ⚡ :: FeedService

--- src/main/java/com/example/unknown/domain/Feed/service/FeedService.java
+++ src/main/java/com/example/unknown/domain/Feed/service/FeedService.java
@@ -3,7 +3,9 @@ package com.example.unknown.domain.Feed.service;
 import com.example.unknown.domain.Feed.presentation.dto.request.ModifyFeedRequest;
 import com.example.unknown.domain.Feed.presentation.dto.request.PostFeedRequest;
 import com.example.unknown.domain.Feed.presentation.dto.response.FeedResponse;
+import com.example.unknown.domain.User.domain.User;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface FeedService {
@@ -17,6 +19,8 @@ public interface FeedService {
     List<FeedResponse> getFeed(int page, int range);
 
     FeedResponse getOneFeed(Long feedId);
+
+    void findAllByCategory(String category, User user, Pageable page);
 
     boolean closeFeed(Long feedId);
 
