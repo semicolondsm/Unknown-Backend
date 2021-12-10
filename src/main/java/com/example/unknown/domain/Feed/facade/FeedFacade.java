9b93bd73eb6d40e35914f1eae5923ad5d5704f89
세현 <lyutvs@gmail.com>
2021-12-10 22:51:18 +0900

    ⚡ :: FeedFacade

--- src/main/java/com/example/unknown/domain/Feed/facade/FeedFacade.java
+++ src/main/java/com/example/unknown/domain/Feed/facade/FeedFacade.java
@@ -3,8 +3,11 @@ package com.example.unknown.domain.Feed.facade;
 import com.example.unknown.domain.Feed.domain.Feed;
 import com.example.unknown.domain.Feed.domain.repository.FeedRepository;
 import com.example.unknown.domain.Feed.exception.FeedNotExistsException;
+import com.example.unknown.domain.Feed.presentation.dto.request.PostFeedRequest;
 import com.example.unknown.domain.Feed.presentation.dto.response.FeedResponse;
 import com.example.unknown.domain.User.domain.User;
+import com.example.unknown.domain.category.domain.Category;
+import com.example.unknown.domain.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class FeedFacade {
 
     private final FeedRepository feedRepository;
+    private final CategoryService categoryService;
 
     public Feed getFeedById(Long feedId) {
         return feedRepository.findFeedById(feedId)
@@ -26,4 +30,12 @@ public class FeedFacade {
                 .description(feed.getDescription())
                 .build();
     }
+
+    public void getCategoryById() {
+
+        Category category = categoryService
+                .findCategoryByName(f);
+
+    }
+
 }
